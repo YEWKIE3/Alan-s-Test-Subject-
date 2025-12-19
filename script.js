e703c964c5af6e0a52d5095ce756fba7ed5ce211
Alan <alannajera3319@gmail.com>
2025-12-18 18:38:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,31 +2,27 @@
 const quotes = [
     { 
         text: "And we're dying with every step we take, we're dying with every breath we make.” ~March to the Sea (Self Titled, 2009)", 
-        explanation: "From the time we're born, we're already dying. It's important to realize that we're not guaranteed the next step or even the next breath. So we have to live while we're dying."
+        explanation: "From the time we're born, we're already dying. We are not guaranteed the next step or breath, so we must live fully while we're alive."
     },
     { 
         text: "Nothing kills a man faster than his own head” ~Trapdoor (Self Titled, 2009)", 
-        explanation: "We're the only ones standing in the way of everything we want."
+        explanation: "We're often the biggest obstacle to our own success and happiness."
     },
     { 
         text: "Please don't be afraid of what your soul is really thinking” ~Isle of Flightless Birds (Self Titled, 2009)", 
-        explanation: "Don't be afraid if your thoughts and opinions don't align with everyone else's. Don't be afraid if you believe in something bigger than yourself."
+        explanation: "It's okay if your thoughts and beliefs differ from others; trust your own perspective."
     },
     { 
         text: "I don't wanna be heard; I want to be listened to” ~Forest (Regional at Best, 2011)", 
-        explanation: "You want people to truly listen to you, not just hear your words and forget them."
-    },
-    { 
-        text: "I don't understand why everything I adore takes a different form when I squint my eyes...” ~Forest (Regional at Best, 2011)", 
-        explanation: "Recognizing that life doesn't always go as planned, and realizing you're not exactly who you thought you would be. This can be a good thing."
+        explanation: "True communication happens when people genuinely listen, not just hear your words."
     },
     { 
         text: "We all are stranger creatures than when we all started out as kids, culture forbids” ~Glowing Eyes (Regional at Best, 2011)", 
-        explanation: "Even though society tries to mold us, embracing our uniqueness is powerful and liberating."
+        explanation: "Embrace your unique self, even if society doesn't understand or allow it."
     },
     { 
         text: "Are you searching for purpose? Then write something, yeah it might be worthless” ~Kitchen Sink (Regional at Best, 2011)", 
-        explanation: "Your purpose is found in creating something meaningful to you, regardless of what others think."
+        explanation: "Purpose can be found in creating something meaningful to you, even if others think it's worthless."
     },
     { 
         text: "While you're doing fine, there's some people and I who have a really tough time getting through this life so excuse us while we sing
